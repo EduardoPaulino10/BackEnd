236524e06d3ef4528c2e0a3f85fd19d69624e683
EduardoPaulino10 <edu.paulino2010>
2026-08-14 15:59:56 -0300

    Atualizando projeto

--- 3-EstruturasCondicionais/ExerciciosCondicionais/exercicio4.php
+++ 3-EstruturasCondicionais/ExerciciosCondicionais/exercicio4.php
@@ -1,10 +1,14 @@
 <?php
 declare(strict_types=1);
+
 $senhaSistema = "SenhaSegura123";
-$cargoUsuario = "Gerente";
-if ($cargoUsuario === "Gerente" && $senhaSistema === "SenhaSegura123") {
+$cargoUsuario = "Auxiliar";
+if (($cargoUsuario ==="Diretor" || $cargoUsuario === "Gerente") && $senhaSistema === "SenhaSegura123") {
     echo "Acesso Permitido";
 } else {
     echo "Acesso Negado";
 }
+
+
+
 ?>
